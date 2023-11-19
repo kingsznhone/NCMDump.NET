d1da85cfc0cd4278ca8f22d51c9dd791d6e0cd71
kingsznhone <35349300+kingsznhone@users.noreply.github.com>
2023-11-19 18:27:41 +0800

    Fix for Win10

--- NCMDumpGUI/MainWindowViewModel.cs
+++ NCMDumpGUI/MainWindowViewModel.cs
@@ -161,10 +161,26 @@ namespace NCMDumpGUI
             }
         }
 
-        private void SwitchTheme() =>
-            ApplicationThemeManager.Apply(
-            ApplicationThemeManager.GetAppTheme() == ApplicationTheme.Dark ? ApplicationTheme.Light : ApplicationTheme.Dark,
-            ApplicationThemeManager.GetAppTheme() == ApplicationTheme.Dark ? WindowBackdropType.Acrylic : WindowBackdropType.Mica);
+        private void SwitchTheme() {
+            var appTheme = ApplicationThemeManager.GetAppTheme();
+            ApplicationTheme newTheme = appTheme == ApplicationTheme.Dark ? ApplicationTheme.Light : ApplicationTheme.Dark;
+
+            WindowBackdropType backdrop = WindowBackdropType.Acrylic;
+            if (System.OperatingSystem.IsWindowsVersionAtLeast(10,0,22000,0))
+            {
+                backdrop = WindowBackdropType.Mica;
+            }
+            else if (System.OperatingSystem.IsWindowsVersionAtLeast(10, 0, 19041, 0))
+            {
+                backdrop = WindowBackdropType.Acrylic;
+            }
+            else
+            {
+                backdrop = WindowBackdropType.None;
+            }
+           
+            ApplicationThemeManager.Apply(newTheme, WindowBackdropType.Acrylic);
+        }
 
         private void ClearList() => NCMCollection.Clear();
     }
