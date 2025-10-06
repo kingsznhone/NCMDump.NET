d4f918349b98a8ce80a0a791cac15991dad85d01
ZNH <35349300+kingsznhone@users.noreply.github.com>
2025-10-07 01:46:37 +0800

    Update NcmRC4.cs

--- src/NCMDump.Core/NcmRC4.cs
+++ src/NCMDump.Core/NcmRC4.cs
@@ -5,15 +5,21 @@
         private readonly byte[] Sbox;
         private byte i = 0, j = 0, Si = 0, Sj = 0;
 
-        public NcmRC4(byte[] key)
+        public unsafe NcmRC4(byte[] key)
         {
-            Sbox = Enumerable.Range(0, 256).Select(i => (byte)i).ToArray();
-
-            //Generate Keybox
-            for (int x = 0, y = 0; x < 256; x++)
+            Sbox = new byte[256];
+            for (int i = 0; i < 256; i++)
             {
-                y = (y + Sbox[x] + key[x % key.Length]) & 0xFF;
-                (Sbox[x], Sbox[y]) = (Sbox[y], Sbox[x]);
+                Sbox[i] = (byte)i;
+            }
+            fixed (byte* sboxPtr = Sbox)
+            {
+                //Generate Keybox
+                for (int x = 0, y = 0; x < 256; x++)
+                {
+                    y = (y + sboxPtr[x] + key[x % key.Length]) & 0xFF;
+                    (sboxPtr[x], sboxPtr[y]) = (sboxPtr[y], sboxPtr[x]);
+                }
             }
         }
 
@@ -25,21 +31,17 @@
 
         public unsafe int Transform(Span<byte> data)
         {
-            fixed (byte* dataPtr = data)
+            fixed (byte* sboxPtr = Sbox)
             {
-                fixed (byte* sboxPtr = Sbox)
-                {
-                    int len = data.Length;
+                int len = data.Length;
 
-                    for (int m = 0; m < len; m++)
-                    {
-                        byte Si = sboxPtr[(byte)(m + 1)];
-                        byte Sj = sboxPtr[(byte)(m + 1 + Si)];
-                        dataPtr[m] ^= sboxPtr[(byte)(Si + Sj)];
-                    }
+                for (int m = 0; m < len; m++)
+                {
+                    byte Si = sboxPtr[(m + 1)&0xFF];
+                    byte Sj = sboxPtr[(m + 1 + Si) & 0xFF];
+                    data[m] ^= sboxPtr[(Si + Sj) & 0xFF];
                 }
             }
-            
             return data.Length;
         }
 
